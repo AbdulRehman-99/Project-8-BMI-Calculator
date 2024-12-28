98ddc51c6d72c03f425533fc89af3699df327465
AbdulRehman-99 <sihamakhan186@gmail.com>
2024-12-28 22:41:56 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+# Project:8 BMI Calculator
+import streamlit as st
+import pandas as pd
+
+st.title("BMI Calculator")
+
+height = st.slider("Enter your height (in cm):", 100, 250, 175)
+weight = st.slider("Enter your weight (in kg):", 40, 200, 70)
+
+bmi = weight / ((height/100) ** 2)
+
+st.write(f"your BMI is {bmi:2f}")
+
+st.write("### BMI Categories ###")
+st.write("_ Underweight: BMI less than 18.5")
+st.write("_ Normal weight: BMI between 18.5 and 24.9")
+st.write("_ Overweight: BMI between 25 and 29.9")
+st.write("_ Obesity: BMI 30 or greater")
